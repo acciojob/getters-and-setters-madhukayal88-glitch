b315b7edeaf716d6bc75dcd564c76e959d02bc64
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-19 10:19:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    // Getter for name
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Getter for age
+    get age() {
+        return this._age;
+    }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+    // Setter for age
+    set age(age) {
+        this._age = age;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    study() {
+        console.log(this.name + " is studying");
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(this.name + " is teaching");
+    }
+}
+
+module.exports = { Person, Student, Teacher };
