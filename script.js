a960797fee5e7daf55b9c8647ba22f7a4b6e54fa
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-19 10:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,38 @@
-// Base Person class
 class Person {
-  constructor(name, age) {
-    this._name = name;  // use underscore for private-like convention
-    this._age = age;
-  }
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-  // Getter for name
-  get name() {
-    return this._name;
-  }
+    // getter for name
+    get name() {
+        return this._name;
+    }
 
-  // Getter for age
-  get age() {
-    return this._age;
-  }
+    // setter for name
+    set name(name) {
+        this._name = name;
+    }
 
-  // Setter for age
-  set age(newAge) {
-    this._age = newAge;
-  }
+    // getter for age
+    get age() {
+        return this._age;
+    }
+
+    // setter for age
+    set age(age) {
+        this._age = age;
+    }
 }
 
-// Student subclass
 class Student extends Person {
-  constructor(name, age) {
-    super(name, age); // call parent constructor
-  }
-
-  study() {
-    console.log(`${this.name} is studying`);
-  }
+    study() {
+        console.log(this.name + " is studying");
+    }
 }
 
-// Teacher subclass
 class Teacher extends Person {
-  constructor(name, age) {
-    super(name, age); // call parent constructor
-  }
-
-  teach() {
-    console.log(`${this.name} is teaching`);
-  }
+    teach() {
+        console.log(this.name + " is teaching");
+    }
 }
-
-// --- Example Usage ---
-// Person
-const person = new Person("John", 25);
-console.log(person.name);   // John
-person.age = 30;
-console.log(person.age);    // 30
-
-// Student
-const student = new Student("Alice", 22);
-student.study();            // Alice is studying
-
-// Teacher
-const teacher = new Teacher("Bob", 40);
-teacher.teach();            // Bob is teaching
