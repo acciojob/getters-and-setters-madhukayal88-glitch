ba0bfaf3e372d30fe7728af29d59d5ba2fb759f4
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-27 05:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 class Person {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-    // getter for name
-    get name() {
-        return this._name;
-    }
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-    // setter for name
-    set name(name) {
-        this._name = name;
-    }
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
 
-    // getter for age
-    get age() {
-        return this._age;
-    }
-
-    // setter for age
-    set age(age) {
-        this._age = age;
-    }
+  // Adding a getter for age as well, as it's usually 
+  // expected when a setter is present
+  get age() {
+    return this._age;
+  }
 }
 
 class Student extends Person {
-    study() {
-        console.log(this.name + " is studying");
-    }
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-    teach() {
-        console.log(this.name + " is teaching");
-    }
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
+// To ensure it works with the Cypress tests and external files:
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
